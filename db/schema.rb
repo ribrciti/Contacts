3de0240d35dfc3ac41f3611a0fe84a882ca88bcf
ribrciti <rbciti@msn.com>
2015-08-11 08:07:32 -0700

    Added sections model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811145300) do
+ActiveRecord::Schema.define(version: 20150811150418) do
 
   create_table "admin_users", force: true do |t|
     t.string   "first_name",      limit: 25
@@ -37,6 +37,19 @@ ActiveRecord::Schema.define(version: 20150811145300) do
 
   add_index "pages", ["permalink"], name: "index_pages_on_permalink", using: :btree
   add_index "pages", ["subject_id"], name: "index_pages_on_subject_id", using: :btree
+
+  create_table "sections", force: true do |t|
+    t.integer  "page_id"
+    t.string   "name"
+    t.integer  "position"
+    t.boolean  "visible",      default: false
+    t.string   "content_type"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sections", ["page_id"], name: "index_sections_on_page_id", using: :btree
 
   create_table "subjects", force: true do |t|
     t.string   "name"
