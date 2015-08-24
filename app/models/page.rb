d102011bb6c949c9bd97a07ea35563e0ece245d6
ribrciti <rbciti@msn.com>
2015-08-24 08:20:21 -0700

    Added Validations to Models

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,15 +2,15 @@ class Page < ActiveRecord::Base
 
 	belongs_to :subject
 	has_many :sections
-	#has_and_belongs_to_many :editors, "class_name" => "AdminUser"
+	has_and_belongs_to_many :editors, "class_name" => "AdminUser"
 
-  validates_presence_of :name
-  validates_length_of :name, :maximum => 255
-  validates_presence_of :permalink
-  validates_length_of :permalink, :maximum => 3..255
+   validates_presence_of :name
+   validates_length_of :name, :maximum => 255
+  #  validates_presence_of :permalink
+  #  validates_length_of :permalink, :maximum => 3..255
   # use presence_of with length_of to disallow spaces
-  validates_uniqueness_of :permalink
-  # for unique values by subject use ":scope => :subject_id"
+  # validates_uniqueness_of :permalink
+          # for unique values by subject use ":scope => :subject_id"
 
 
 	scope :visible, lambda { where(:visible => true) }
